fe3e9520dc530d3461ac9e8fe9a6a29a163e84ee
Rohan Shirke <rohanshirke25812@gmail.com>
2023-01-14 22:05:14 +0530

    implementation

--- Implementation of Core DS/QueueUsingArray.java
+++ Implementation of Core DS/QueueUsingArray.java
@@ -4,20 +4,20 @@ public class QueueUsingArray {
 
     public static class Queue{
     
-        int front;
-        int rear;
         int queue[];
+        int size;
+        int front = -1;
+        int rear = -1;
     
         // constructor
         public Queue(int capacity) {
-            queue = new int[capacity];
-            front = -1;
-            rear = -1;
+            this.size = capacity;
+            queue = new int[size];
         }
     
         // checking overflow
         public boolean overflow() {
-            if(rear == queue.length - 1){
+            if(rear >= (size - 1)){
                 return true;
             }
             return false;
@@ -40,7 +40,12 @@ public class QueueUsingArray {
                 System.out.print("Enter data : ");
                 int data = sc.nextInt();
                 if(front == -1 && rear == -1){
-                    front = rear = 0;
+                    front = 0;
+                    rear = 0;
+                    queue[rear] = data;
+                }
+                else if(front == 0 && rear == 0){
+                    rear = rear + 1;
                     queue[rear] = data;
                 }
                 else{
@@ -68,38 +73,51 @@ public class QueueUsingArray {
             if(underflow()){
                 System.out.println("\nQueue is Empty");
             }
-            else {
-                System.out.print("Queue : ");
+            else if(front<=rear) {
+                System.out.print("\nQueue : ");
                 for (int i = front; i <= rear; i++) {
                     System.out.print(queue[i] + " ");
                 }
                 System.out.println();
-                System.out.println("Front => " + queue[front]);
-                System.out.println("Rear => " + queue[rear]);
+                System.out.println("Front => " + queue[front] + "  &&  Rear => " + queue[rear]);
             }
         }
     
         // check elements present in queue
         public void size(){
-            System.out.println("\nNumber of Elements in Queue : " + (rear - front));
+            if(underflow()){
+                System.out.println("\nNumber of Elements in Queue : " + (0));
+            }
+            else{
+                System.out.println("\nNumber of Elements in Queue : " + (rear - front + 1));
+            }
         }
+
+        // Restoring the queue
+        public void restartQueue() {
+            front = -1;
+            rear = -1;
+            System.out.println("\nQueue Restarted Successfully");
+        }
+
     
     }
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-        System.out.println("\nWELCOME TO QUEUE DATA STRUCTURE USING ARRAY");
+        	System.out.println("\nWELCOME TO QUEUE DATA STRUCTURE USING ARRAY\n");
 		System.out.print(("Enter the size of the Queue : "));
 		int n = sc.nextInt();
 		Queue que = new Queue(n);
 		do {
-            System.out.println();
+            		System.out.println();
 			System.out.print("Which operation do you want to perform on Queue?\n"
 					+ "Press 1 for Enqueue()\n"
 					+ "Press 2 for Dequeue()\n"
 					+ "Press 3 for Display()\n"
                     + "Press 4 for Size()\n"
-                    + "Press 5 for EXIT()\n"
+                    + "Press 5 for Restart Queue()\n"
+                    + "Press 6 for EXIT()\n"
                     + "\nYour Choice :) ");
 			int operation = sc.nextInt();
 			switch(operation) {
@@ -108,26 +126,30 @@ public class QueueUsingArray {
 				break;
 			
 			case 2 :
-                que.dequeue();
+                		que.dequeue();
 				break;
 				
 			case 3 :
-                que.display();
+                		que.display();
 				break;
             
-            case 4 :
-                que.size();
-                break;
+            		case 4 :
+                		que.size();
+                		break;
 
-            case 5 :
-                System.out.println("Exit Successfully (^__^)");
-                System.exit(0);
+            		case 5 :
+                		que.restartQueue();
+                		break;
+
+            		case 6 :
+                		System.out.println("Exit Successfully (^__^)");
+                		System.exit(0);
 
 			default :
 				System.out.println("Invalid choice!");
 			}
 		}
-        while(true);
+        	while(true);
 	}
 
 }
